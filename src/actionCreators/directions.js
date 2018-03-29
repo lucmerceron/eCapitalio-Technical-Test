e4891c28712b78e0dec721588e68ca6da9e864d8
Luc Merceron <luc.merceron.developer@gmail.com>
2018-03-29 14:58:20 +0200

    searchDirections action implementation

---
+++ src/actionCreators/directions.js
@@ -0,0 +1,59 @@
+import makeActionCreator from './makeActionCreator'
+import Api from '../service/Api'
+import constants from '../constants/constants'
+import searchForClosestStation from '../utils/searchForClosestStation'
+
+/* Scoped to this module */
+const DirectionsService = new window.google.maps.DirectionsService()
+const WALKING = window.google.maps.TravelMode.WALKING
+const BICYCLING = window.google.maps.TravelMode.BICYCLING
+
+/* Action types */
+export const SEARCH_DIRECTIONS_REQUEST = 'SEARCH_DIRECTIONS_REQUEST'
+export const SEARCH_DIRECTIONS_SUCCESS = 'SEARCH_DIRECTIONS_SUCCESS'
+export const SEARCH_DIRECTIONS_FAILED = 'SEARCH_DIRECTIONS_FAILED'
+
+/* Action creators */
+export const searchDirectionsRequest = makeActionCreator(
+  SEARCH_DIRECTIONS_REQUEST,
+  'departureLatLng',
+  'arrivalLatLng',
+  'departureStation',
+  'arrivalStation'
+)
+export const searchDirectionsSuccess = makeActionCreator(
+  SEARCH_DIRECTIONS_SUCCESS,
+  'departureToStationDirections',
+  'stationToStationDirections',
+  'stationToArrivalDirections'
+)
+export const searchDirectionsFailed = makeActionCreator(SEARCH_DIRECTIONS_FAILED, 'error')
+
+/* Thunk action creators */
+export function searchDirections(departureLatLng, arrivalLatLng, departureStation, arrivalStation) {
+  return dispatch => {
+    dispatch(
+      searchDirectionsRequest(departureLatLng, arrivalLatLng, departureStation, arrivalStation)
+    )
+    Promise.all([
+      retrieveDirection(departureLatLng, getLatLngOfStation(departureStation), WALKING),
+      retrieveDirection(getLatLngOfStation(departureLatLng), getLatLngOfStation(arrivalStation), BICYCLING),
+      retrieveDirection(getLatLngOfStation(arrivalStation), arrivalLatLng, WALKING),
+    ]).then(directions => {
+      searchDirectionsSuccess(...directions)
+    }).catch(searchDirectionsFailed)
+  }
+}
+
+const getLatLngOfStation = station => { lat: station.lat, lng: station.lng }
+
+const retrieveDirection = (origin, destination, travelMode) =>
+  new Promise((resolve, reject) => {
+    DirectionsService.route({ origin, destination, travelMode }, (directions, status) => {
+      if (status === window.google.maps.DirectionsStatus.OK) {
+        resolve(directions)
+      } else {
+        reject('Sorry, we were unable to calculate the directions, please try another route.')
+      }
+    })
+  })
