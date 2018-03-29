d2fc3131a7f8304e0e818a1b4555e8568f2a4088
Luc Merceron <luc.merceron.developer@gmail.com>
2018-03-29 11:48:44 +0200

    Redux thunk for async action

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,10 +1,13 @@
 import { createStore, applyMiddleware } from 'redux'
 import { createLogger } from 'redux-logger'
+import thunk from 'redux-thunk'
 
 import rootReducer from '../reducers/rootReducer'
 
 const middlewares =
-  process.env.NODE_ENV === 'production' ? applyMiddleware() : applyMiddleware(createLogger())
+  process.env.NODE_ENV === 'production'
+    ? applyMiddleware(thunk)
+    : applyMiddleware(thunk, createLogger())
 
 export default function configureStore(preloadedState = {}) {
   return createStore(rootReducer, preloadedState, middlewares)
