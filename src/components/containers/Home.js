6d3fb425fb3ee20a8334b4b51069dcb56f1efb90
Luc Merceron <luc.merceron.developer@gmail.com>
2018-03-29 11:19:36 +0200

    Pass state & actions as props to Home presentational

--- src/components/containers/Home.js
+++ src/components/containers/Home.js
@@ -1,9 +1,15 @@
 import { connect } from 'react-redux'
 
+import {
+  searchClosestDepartureBike,
+  searchClosestArrivalDock
+} from '../../actionCreators/velibStation'
 import Home from '../presentationals/Home'
 
-const mapStateToProps = state => ({})
+const mapStateToProps = state => ({
+  velibStation: state.velibStation
+})
 
-const mapDispatchToProps = dispatch => ({})
+const mapDispatchToProps = { searchClosestDepartureBike, searchClosestArrivalDock }
 
 export default connect(mapStateToProps, mapDispatchToProps)(Home)
