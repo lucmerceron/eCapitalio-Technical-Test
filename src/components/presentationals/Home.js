7470188ed584a3cceab9219a76d89363deaa4922
Luc Merceron <luc.merceron.developer@gmail.com>
2018-03-29 11:23:19 +0200

    Passing down props from home pres to autocomplete & map components

--- src/components/presentationals/Home.js
+++ src/components/presentationals/Home.js
@@ -1,21 +1,46 @@
 import React from 'react'
-// import PropTypes from 'prop-types'
+import PropTypes from 'prop-types'
 import MapContainer from './MapContainer'
 import AutocompleteInput from './AutocompleteInput'
 import './Home.css'
 
-const Home = props => (
+const Home = ({ searchClosestDepartureBike, searchClosestArrivalDock, velibStation }) => (
   <div className="bike-my-way">
     Bike My Way
-    <AutocompleteInput />
+    <AutocompleteInput
+      handleBikeSearch={({ departureLatLng, arrivalLatLng }) => {
+        searchClosestDepartureBike(departureLatLng.lat, departureLatLng.lng)
+        searchClosestArrivalDock(arrivalLatLng.lat, arrivalLatLng.lng)
+      }}
+    />
     <MapContainer
       loadingElement={<div style={{ height: `100%` }} />}
       containerElement={<div className="bike-my-way-map-container" />}
       mapElement={<div style={{ height: `100%` }} />}
+      velibStation={velibStation}
     />
   </div>
 )
 
-Home.propTypes = {}
+Home.propTypes = {
+  velibStation: PropTypes.shape({
+    departureStation: PropTypes.shape({
+      name: PropTypes.string,
+      lat: PropTypes.number,
+      lng: PropTypes.number,
+      nbEbike: PropTypes.number,
+      distance: PropTypes.number
+    }),
+    arrivalStation: PropTypes.shape({
+      name: PropTypes.string,
+      lat: PropTypes.number,
+      lng: PropTypes.number,
+      nbFreeEDock: PropTypes.number,
+      distance: PropTypes.number
+    })
+  }).isRequired,
+  searchClosestDepartureBike: PropTypes.func.isRequired,
+  searchClosestArrivalDock: PropTypes.func.isRequired
+}
 
 export default Home
