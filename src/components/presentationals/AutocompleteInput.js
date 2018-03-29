f2d4b5ef982b6f632674667adb16c28f60b10178
Luc Merceron <luc.merceron.developer@gmail.com>
2018-03-29 09:23:24 +0200

    Autocomplete logic

--- src/components/presentationals/AutocompleteInput.js
+++ src/components/presentationals/AutocompleteInput.js
@@ -1,11 +1,34 @@
 import React from 'react'
 import PlacesAutocomplete, { geocodeByAddress, getLatLng } from 'react-places-autocomplete'
 
+import './AutocompleteInput.css'
+
 class AutocompleteInput extends React.Component {
   constructor(props) {
     super(props)
-    this.state = { address: 'San Francisco, CA' }
-    this.onChange = address => this.setState({ address })
+    this.state = {
+      departure: '',
+      departureId: '',
+      arrival: '',
+      arrivalId: '',
+      bothDestinationsSelected: 0 // 0: none selected, 1: one selected, 2 or more: both selected
+    }
+    this.onDepartureChange = departure => this.setState({ departure })
+    this.onArrivalChange = arrival => this.setState({ arrival })
+    this.onDepartureSelected = (address, placeId) => {
+      this.setState(prevState => ({
+        bothDestinationsSelected: prevState.bothDestinationsSelected + 1,
+        departure: address,
+        departureId: placeId
+      }))
+    }
+    this.onArrivalSelected = (address, placeId) => {
+      this.setState(prevState => ({
+        bothDestinationsSelected: prevState.bothDestinationsSelected + 1,
+        arrival: address,
+        arrivalId: placeId
+      }))
+    }
   }
 
   handleFormSubmit = event => {
@@ -18,15 +41,39 @@ class AutocompleteInput extends React.Component {
   }
 
   render() {
-    const inputProps = {
-      value: this.state.address,
-      onChange: this.onChange
-    }
-
+    const { departure, arrival, bothDestinationsSelected } = this.state
+    console.log(this.state)
     return (
-      <form onSubmit={this.handleFormSubmit}>
-        <PlacesAutocomplete inputProps={inputProps} />
-        <button type="submit">Submit</button>
+      <form onSubmit={this.handleFormSubmit} className="bike-my-way-form">
+        <PlacesAutocomplete
+          inputProps={{
+            value: departure,
+            onChange: this.onDepartureChange,
+            placeholder: 'Where are you ?'
+          }}
+          onSelect={this.onDepartureSelected}
+          classNames={{
+            input: 'bike-my-way-input',
+            autocompleteContainer: 'bike-my-way-input-container'
+          }}
+          debounce={200}
+        />
+        <PlacesAutocomplete
+          inputProps={{
+            value: arrival,
+            onChange: this.onArrivalChange,
+            placeholder: 'Where are you going ?'
+          }}
+          onSelect={this.onArrivalSelected}
+          classNames={{
+            input: 'bike-my-way-input',
+            autocompleteContainer: 'bike-my-way-input-container'
+          }}
+          debounce={200}
+        />
+        <button type="submit" disabled={bothDestinationsSelected < 2}>
+          Find my Bike !
+        </button>
       </form>
     )
   }
