1177568987c10cee6e840e8545f011b2c689876d
Luc Merceron <luc.merceron.developer@gmail.com>
2018-03-29 08:40:33 +0200

    Map installation

--- src/components/presentationals/Home.js
+++ src/components/presentationals/Home.js
@@ -1,9 +1,19 @@
 import React from 'react'
 // import PropTypes from 'prop-types'
-
+import MapContainer from './MapContainer'
 import './Home.css'
 
-const Home = () => <div className="bike-my-way">Home view</div>
+const Home = props => (
+  <div className="bike-my-way">
+    Home view
+    <MapContainer
+      googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places"
+      loadingElement={<div style={{ height: `100%` }} />}
+      containerElement={<div style={{ height: `400px` }} />}
+      mapElement={<div style={{ height: `100%` }} />}
+    />
+  </div>
+)
 
 Home.propTypes = {}
 
---
+++ src/components/presentationals/MapContainer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+// import PropTypes from 'prop-types'
+import { withScriptjs, withGoogleMap, GoogleMap, DirectionsRenderer } from 'react-google-maps'
+
+const MapContainer = props => (
+  <GoogleMap defaultZoom={15} defaultCenter={{ lat: 41.852183, lng: -87.675462 }} />
+)
+
+MapContainer.propTypes = {}
+
+export default withScriptjs(withGoogleMap(MapContainer))
